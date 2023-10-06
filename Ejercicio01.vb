0ef8720b27eb73d6adb0f6bedb014de30e76acc8
Aleanzora012022 <142461939+Aleanzora012022@users.noreply.github.com>
2023-10-05 18:41:09 -0600

    Add files via upload

---
+++ Ejercicio01.vb
@@ -0,0 +1,50 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("Bienvenido/a")
+
+        While True
+            Console.Write("Ingrese el precio del artículo: ")
+            Dim precioArticulo As Decimal
+            Dim dineroEntregado As Decimal
+
+            Dim input As String = Console.ReadLine()
+            If Not Decimal.TryParse(input, precioArticulo) Then
+                Console.WriteLine("Error de entrada, favor de ingresar un número válido")
+                Continue While
+            End If
+
+            If precioArticulo < 0 Then
+                Exit While
+            End If
+
+            Console.Write("Ingresar la cantidad de dinero dada por el cliente: ")
+            input = Console.ReadLine()
+            If Not Decimal.TryParse(input, dineroEntregado) Then
+                Console.WriteLine("Error de entrada, favor de ingresar un número válido.")
+                Continue While
+            End If
+
+            If dineroEntregado > precioArticulo Then
+                Dim cambio As Decimal = dineroEntregado - precioArticulo
+                Console.WriteLine("Cambio a entregar: $" & cambio.ToString("0.00"))
+                Console.ReadLine()
+            ElseIf dineroEntregado = precioArticulo Then
+                Console.WriteLine("Entrega del producto completada")
+                Console.WriteLine("Gracias por su compra")
+                Console.WriteLine("---------------------------------------------")
+                Console.WriteLine("Laboratorio 2 realizado por Monica Alejandra Anzora Velasquez y Daniela Beatriz Rivas Hernandez")
+                Console.ReadLine()
+            Else
+                Console.WriteLine("Debe pagar $" & (precioArticulo - dineroEntregado).ToString("0.00") & " más")
+                Console.WriteLine("No posee suficiente dinero, vuelva pronto por su producto")
+                Console.WriteLine("---------------------------------------------")
+                Console.WriteLine("Laboratorio 2 realizado por Monica Alejandra Anzora Velasquez y Daniela Beatriz Rivas Hernandez")
+                Console.ReadLine()
+            End If
+        End While
+
+    End Sub
+
+End Module
---
+++ Ejercicio02.vb
@@ -0,0 +1,48 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim totalCompra As Decimal = 0
+
+        Console.WriteLine("Bienvenido/a")
+
+        While True
+            Console.Write("Ingresar el monto de la compra: ")
+            Dim montoCompra As Decimal
+
+            Dim input As String = Console.ReadLine()
+            If Not Decimal.TryParse(input, montoCompra) Then
+                Console.WriteLine("Error de entrada, favor de ingresar un número valido")
+                Continue While
+            End If
+
+            If montoCompra < 0 Then
+                Exit While
+            End If
+
+            Dim descuento As Decimal = 0
+
+            If montoCompra > 1000 Then
+                descuento = montoCompra * 0.1
+            ElseIf montoCompra > 500 Then
+                descuento = montoCompra * 0.05
+            End If
+
+            Dim precioFinal As Decimal = montoCompra - descuento
+            totalCompra += precioFinal
+
+            Console.WriteLine("Subtotal: $" & montoCompra.ToString("0.00"))
+            Console.WriteLine("EL descuento que se aplico: $" & descuento.ToString("0.00"))
+            Console.WriteLine("El total a pagar: $" & precioFinal.ToString("0.00"))
+            Console.WriteLine("Resumen de la compra:")
+            Console.WriteLine("Total de la compra: $" & totalCompra.ToString("0.00"))
+            Console.WriteLine("Gracias por su compra")
+
+            Console.WriteLine("------------------------")
+            Console.WriteLine("Laboratorio 2 realizado por Daniela Beatriz Rivas Hernandez y Monica Alejandra Anzora Velasquez")
+            Console.ReadLine()
+
+        End While
+
+    End Sub
+
+End Module
